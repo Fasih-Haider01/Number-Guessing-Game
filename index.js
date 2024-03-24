4f6baadfa4d23d9c53c9d571ff03056ca26fbb28
Fasih-Haider01 <fasihhaider105@gmail.com>
2024-03-24 14:41:23 -0700

    committing whole program

---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a number
+// 2) user will input a value for guessing number
+// 3) compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("WELCOME!");
+const asnwer = await inquirer.prompt([
+    {
+        message: "please guess a number between 1-6",
+        type: "number",
+        name: "UserGuessedNumber",
+    },
+]);
+if (asnwer.UserGuessedNumber === randomNumber) {
+    console.log("CONGRATULATIONS! You guessed the right number");
+}
+else {
+    console.log("You guessed the wrong number");
+}
+console.log("THE END");
