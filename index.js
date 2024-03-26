88c5b68ab777ffbbb44008d058edadbbea3c18ab
Fasih-Haider01 <fasihhaider105@gmail.com>
2024-03-26 14:33:13 -0700

    added for loop

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ import inquirer from "inquirer";
 // 1) computer will generate a number
 // 2) user will input a value for guessing number
 // 3) compare user input with computer generated number and show result
-const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log("WELCOME!");
-const asnwer = await inquirer.prompt([
-    {
-        message: "please guess a number between 1-6",
-        type: "number",
-        name: "UserGuessedNumber",
-    },
-]);
-if (asnwer.UserGuessedNumber === randomNumber) {
-    console.log("CONGRATULATIONS! You guessed the right number");
+let guessedCorrectly = false;
+let randomNumber;
+for (let i = 0; i < 4; i++) {
+    randomNumber = Math.floor(Math.random() * 6 + 1);
+    const asnwer = await inquirer.prompt([
+        {
+            message: "please guess a number between 1-6",
+            type: "number",
+            name: "UserGuessedNumber",
+        },
+    ]);
+    if (asnwer.UserGuessedNumber === randomNumber) {
+        console.log("CONGRATULATIONS! You guessed the right number");
+        guessedCorrectly = true;
+        break;
+    }
+    else {
+        console.log("You guessed the wrong number. Chances Left:" + (3 - i));
+    }
 }
-else {
-    console.log("You guessed the wrong number");
+if (!guessedCorrectly) {
+    console.log("Sorry, you've run out of chances. The correct number was:", randomNumber);
 }
 console.log("THE END");
--- index.ts
+++ index.ts
@@ -6,8 +6,13 @@ import inquirer from "inquirer";
 // 2) user will input a value for guessing number
 // 3) compare user input with computer generated number and show result
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log( "WELCOME!");
+let guessedCorrectly = false;
+let randomNumber;
+
+for(let i=0; i<4; i++)
+{
+randomNumber = Math.floor(Math.random() * 6 + 1);
 const asnwer = await inquirer.prompt([
   {
     message: "please guess a number between 1-6",
@@ -19,9 +24,15 @@ const asnwer = await inquirer.prompt([
 if( asnwer.UserGuessedNumber === randomNumber)
 {
   console.log( "CONGRATULATIONS! You guessed the right number");
-}
+  guessedCorrectly = true;
+  break; }
 else
 {
-  console.log ( "You guessed the wrong number");
+  console.log ( "You guessed the wrong number. Chances Left:"+ (3-i));}
 }
+if (!guessedCorrectly) {
+  console.log("Sorry, you've run out of chances. The correct number was:" , randomNumber);
+}
+
+
 console.log( "THE END");
